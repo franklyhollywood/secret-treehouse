fef633ea783fe530b292050877d35218513dfb6e
Frank Hollywood <github@srvsync.com>
2022-01-03 13:07:51 -0800

    first and last commit

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -19,6 +19,10 @@ export default function Login() {
     event.preventDefault();
     const loginWasSuccessful = auth.login(formState.email, formState.password);
 
+    !loginWasSuccessful
+      ? setError('Enter proper email and password')
+      : history.replace('/');
+
     // TODO: If login was unsuccessful, set an error with a message
     // to display to the user that their login failed.
     //
@@ -32,17 +36,19 @@ export default function Login() {
     <>
       <h3>You must log in to view the page at {from.pathname}</h3>
       <form onSubmit={handleLogin} className={styles.loginForm}>
-        <label>Email</label>
+        <label htmlFor="email">Email</label>
         <input
           id="email"
           name="email"
           type="email"
+          onChange={(event) => handleFormChange(event)}
         />{' '}
-        <label>Password</label>
+        <label htmlFor="password">Password</label>
         <input
           id="password"
           name="password"
           type="password"
+          onChange={(event) => handleFormChange(event)}
         />
         <button type="submit" aria-label="Sign In">
           Sign in
